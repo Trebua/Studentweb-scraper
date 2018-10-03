9d59d1693efafd185e6e203c757c4d37dfff9656
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-10-03 11:14:20 +0200

    Headless running and adjustments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Studentweb-scraper
 Script for automatically logging in to studentweb and retrieving any new or updated grades
+
+### Prerequisites
+* [Download chromedriver](https://chromedriver.storage.googleapis.com/index.html?path=2.42/)
+* pip install selenium
+* pip install numpy
--- scraper.py
+++ scraper.py
@@ -1,28 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import ast
 import sys
 import numpy as np
 from collections import Counter
 
-url = "https://idp.feide.no/simplesaml/module.php/feide/login.php?asLen=169&AuthState=_41e67986da33cb1be903b3796b1c61ba1118d6230c%3Ahttps%3A%2F%2Fidp.feide.no%2Fsimplesaml%2Fsaml2%2Fidp%2FSSOService.php%3Fspentityid%3Dhttps%253A%252F%252Ffsweb.no%252Fstudentweb%26cookieTime%3D1538497429"
+#SETUP
 my_username = "" #WRITE YOUR FEIDE-USERNAME HERE
 my_password = "" #WRITE YOUR FEIDE-PASSWORD HERE
 my_school = "NTNU" #WRITE YOUR SCHOOL NAME HERE
 my_school_fullname = "norges teknisk-naturvitenskap" #WRITE THE FULL NAME OF YOUR SCHOOL HERE
 
-driver = webdriver.Chrome()
+url = "https://idp.feide.no/simplesaml/module.php/feide/login.php?asLen=169&AuthState=_41e67986da33cb1be903b3796b1c61ba1118d6230c%3Ahttps%3A%2F%2Fidp.feide.no%2Fsimplesaml%2Fsaml2%2Fidp%2FSSOService.php%3Fspentityid%3Dhttps%253A%252F%252Ffsweb.no%252Fstudentweb%26cookieTime%3D1538497429"
+
+#Uncomment to see the procedure live in a Chrome-browser:
+#driver = webdriver.Chrome()
+
+#To make the procedure invisible:
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(chrome_options=options)
+
 driver.get(url)
 ac = webdriver.ActionChains(driver)
 
-#DEL 1 - Velg skole
+#PART 1 - CHOOSE SCHOOL
 schoolpath = "//input[@id='org_selector-selectized']"
-driver.find_element_by_xpath(schoolpath).send_keys(my_school)
-driver.find_element_by_xpath(schoolpath).send_keys(Keys.ENTER)
+driver.find_element_by_xpath(schoolpath).send_keys(my_school,Keys.ENTER)
 
-
-#DEL 2 - Logg inn på studentweb
+#PART 2 - LOG IN TO STUDENTWEB WITH FEIDE-CREDENTIALS
 loginpaths =  { 
     'username' : "//input[@id='username']",
     'password' : "//input[@id='password']",
@@ -31,19 +37,19 @@ driver.find_element_by_xpath(loginpaths['username']).send_keys(my_username)
 password_field = driver.find_element_by_xpath(loginpaths['password'])
 password_field.send_keys(my_password, Keys.ENTER)
 
-#DEL 3 - Velge skole igjen!?  
+#PART 3 - CHOOSE SCHOOL AGAIN FROM NEW LIST
 element = driver.find_element_by_id("institusjonsvalg")
 ac.move_to_element(element).click().perform()
 ac.send_keys(my_school_fullname, Keys.ENTER)
 ac.move_to_element(element).move_by_offset(70,0).click().perform()
 
-#DEL 4 - Trykk logg inn med feide eeeeen gang til
+#PART 4 - CLICK "LOG ON USING FEIDE"
 driver.find_element_by_link_text('Log on using Feide').click()
 
-#DEL 5 - Åpner karaktersiden
+#PART 5 - OPEN URL WITH GRADES
 driver.get("https://fsweb.no/studentweb/resultater.jsf")
 
-#DEL 6 - Hent karakterer
+#PART 6 - EXTRACT GRADES
 table = driver.find_element_by_id('resultatlisteForm:HeleResultater:resultaterPanel')
 table_body = table.find_elements_by_tag_name("tbody")[0]
 
@@ -64,32 +70,34 @@ def create_dict(rows):
         results[emne_code] = [emne_name,grade]
     return results
 
+#PART 7 - FILE-HANDLING AND COMPARING PREVIOUS RESULTS WITH NEW
 def write_results_to_file(results, filename):
     with open(filename, 'w') as f:
         f.write(str(results))
 
 def read_results_from_file(filename):
     with open(filename, 'r') as f:
-        return ast.literal_eval(f.read())
+        return eval(f.read())
 
 def compare_results(prev, new):
     prev_keys = prev.keys()
     new_keys = new.keys()
-    if len(new_keys) != len(prev_keys): #Hvis ny karakter har kommet
+    changed = False
+    if len(new_keys) != len(prev_keys): #IF NEW GRADE
         diff = np.setdiff1d(list(new_keys),list(prev_keys))
         for key in diff:
-            print(f"Fått karakter i {key} {new[key][0]}:{new[key][1]}")
-    for key in prev: #Hvis en karakter er endret
-        if prev[key][1] != new[key][1]:
-            print(f"Endret karakter i {key} {prev[key][0]}: {new[key][1]}")
-    print("Sammenligning ferdig")
+            changed = True
+            print(f"Ny karakter i {key} {new[key][0]}: {new[key][1]}")
+    for key in prev: #IF GRADE IS CHANGED (Ignore passed/not passed b/c of unicode-issues)
+        if prev[key][1] != new[key][1] and not "bes" in prev[key][1].lower():
+            changed = True
+            print(f"Endret karakter i {key} {prev[key][0]}: {prev[key][1]} => {new[key][1]}")
+    print("Ingen endringer") if not changed else print("Ferdig")
 
 new = create_dict(rows)
-#read fra file først her egentlig
-write_results_to_file(new, "previous_results.txt")
-prev = read_results_from_file("test.txt")
+prev = read_results_from_file("results.txt")
+write_results_to_file(new, "results.txt")
 compare_results(prev,new)
 
-
-
-
+driver.close()
+driver.quit()
